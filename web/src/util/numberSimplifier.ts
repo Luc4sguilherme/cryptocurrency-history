f947868d90115c6a05d0aa339371192cfd10050d
Luc4sguilherme <lucas.guilherme.matutina@gmail.com>
2022-06-27 11:50:54 -0300

    add currency value format based on browser language

--- web/src/util/numberSimplifier.ts
+++ web/src/util/numberSimplifier.ts
@@ -1,5 +1,5 @@
 export default function numberSimplifier(number: number, precision: number) {
-  return new Intl.NumberFormat('en-US', {
+  return new Intl.NumberFormat(window.navigator.language, {
     notation: 'compact',
     compactDisplay: 'short',
     maximumFractionDigits: precision,
